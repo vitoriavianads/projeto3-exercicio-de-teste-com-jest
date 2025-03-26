37cb7221f294c3dcc7595c65d77b880208c28fcc
Vitória Viana <159632505+vitoriavianads@users.noreply.github.com>
2025-03-26 16:48:52 -0300

    Update calculadora.js

--- __tests__/src/calculadora.js
+++ __tests__/src/calculadora.js
@@ -1,20 +1,13 @@
-// Subtrair 
-
-
 function subtrai(a, b) {
-    return a - b;
-}
-
-//Par ou não
-
-function ehPar(n) {
-    return n % 2 == 0
-}
-
-// Contar Caracteres
-
-function contarCaracteres(texto) {
+    return a - b
+  }
+  
+  function ehPar(n) {
+    return n % 2 === 0
+  }
+  
+  function contarCaracteres(texto) {
     return texto.length
-}
-
-module.exports = { subtrai, ehPar, contarCaracteres};
+  }
+  
+  module.exports = { subtrai, ehPar, contarCaracteres }
