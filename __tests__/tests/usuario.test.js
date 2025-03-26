aa53893508a2eeb09d9e6cef8212cd01cfbbd4f1
Vitória Viana <159632505+vitoriavianads@users.noreply.github.com>
2025-03-26 16:27:06 -0300

    Update usuario.test.js

--- __tests__/tests/usuario.test.js
+++ __tests__/tests/usuario.test.js
@@ -1,21 +1,21 @@
 const { usuario } = require('../src/usuario')
 
 describe('Usuário', () => {
-    test('Propriedades', () => {
-        expect(usuario).toHaveProperty('nome')
-        expect(usuario).toHaveProperty('idade')
-        expect(usuario).toHaveProperty('email')
-    })
+    // test('Propriedades', () => {
+    //     expect(usuario).toHaveProperty('nome')
+    //     expect(usuario).toHaveProperty('idade')
+    //     expect(usuario).toHaveProperty('email')
+    // })
 
-    test('Tipos de propriedades', () => {
-        expect(typeof usuario.nome).toBe('string')
-        expect(typeof usuario.idade).toBe('number')
-        expect(typeof usuario.email).toBe('string')
-    })
+    // test('Tipos de propriedades', () => {
+    //     expect(typeof usuario.nome).toBe('string')
+    //     expect(typeof usuario.idade).toBe('number')
+    //     expect(typeof usuario.email).toBe('string')
+    // })
 
     test('As propriedades devem ter valores esperados', () => {
-        expect(usuario.nome).toBe('Ana Vitoria')
+        expect(usuario.nome).toBe('Ray Santiago')
         expect(usuario.idade).toBe(18)
-        expect(usuario.email).toBe('ana.mesquita129@aluno.ce.gov.br')
+        expect(usuario.email).toBe('jose.gomes360@aluno.ce.gov.br')
     })
 })
