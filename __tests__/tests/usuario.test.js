09849f7baf0eb2348e376fa5f5c0990e1efdc6da
Vitória Viana <159632505+vitoriavianads@users.noreply.github.com>
2025-03-26 16:41:52 -0300

    Update usuario.test.js

--- __tests__/tests/usuario.test.js
+++ __tests__/tests/usuario.test.js
@@ -14,8 +14,8 @@ describe('Usuário', () => {
     // })
 
     test('As propriedades devem ter valores esperados', () => {
-        expect(usuario.nome).toBe('Ray Santiago')
+        expect(usuario.nome).toBe('Ana Vitória')
         expect(usuario.idade).toBe(18)
-        expect(usuario.email).toBe('jose.gomes360@aluno.ce.gov.br')
+        expect(usuario.email).toBe('ana.mesquita129@aluno.ce.gov.br')
     })
 })
