c5d2bdee9f73f5ea45168ec0ab2d03c0f99e5423
Vitória Viana <159632505+vitoriavianads@users.noreply.github.com>
2025-03-26 16:49:51 -0300

    Update usuario.test.js

--- __tests__/tests/usuario.test.js
+++ __tests__/tests/usuario.test.js
@@ -1,21 +1,15 @@
-const { usuario } = require('../src/usuario')
+const usuario = require('../../../src/usuario');
 
-describe('Usuário', () => {
-    // test('Propriedades', () => {
-    //     expect(usuario).toHaveProperty('nome')
-    //     expect(usuario).toHaveProperty('idade')
-    //     expect(usuario).toHaveProperty('email')
-    // })
+describe('Testes do Usuário', () => {
+  test('Usuário tem nome, idade e email', () => {
+    expect(usuario).toHaveProperty('nome')
+    expect(usuario).toHaveProperty('idade')
+    expect(usuario).toHaveProperty('email')
+  });
 
-    // test('Tipos de propriedades', () => {
-    //     expect(typeof usuario.nome).toBe('string')
-    //     expect(typeof usuario.idade).toBe('number')
-    //     expect(typeof usuario.email).toBe('string')
-    // })
-
-    test('As propriedades devem ter valores esperados', () => {
-        expect(usuario.nome).toBe('Ana Vitória')
-        expect(usuario.idade).toBe(18)
-        expect(usuario.email).toBe('ana.mesquita129@aluno.ce.gov.br')
-    })
-})
+  test('Propriedades do usuário são do tipo correto', () => {
+    expect(typeof usuario.nome).toBe('string')
+    expect(typeof usuario.idade).toBe('number')
+    expect(typeof usuario.email).toBe('string')
+  });
+});
